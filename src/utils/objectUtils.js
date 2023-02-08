a7197471fdc71510a80bbc76cb7b7fd997fe1b96
Facundo Pohle <facundopohle@gmail.com>
2023-02-08 15:59:38 -0300

    desafio14

--- src/utils/objectUtils.js
+++ src/utils/objectUtils.js
@@ -1,3 +1,35 @@
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import bCrypt from 'bcrypt'
+import config from '../config.js'
+
+function createOnMongoStore() {
+  const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+  return session({
+    store: MongoStore.create({
+      mongoUrl:
+        config.mongoRemote.cnxStr,
+      mongoOptions: advancedOptions,
+      ttl: 120,
+      collectionName: 'sessions',
+    }),
+    secret: 'anarchy123',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 },
+  })
+}
+
+function createHash(password) {
+  return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null)
+}
+
+function isValidPassword(user, password) {
+  return bCrypt.compareSync(password, user.password)
+}
+
+export default {createOnMongoStore, createHash, isValidPassword}
+
 export const asPOJO = obj => JSON.parse(JSON.stringify(obj))
 
 export const renameField = (record, from, to) => {
